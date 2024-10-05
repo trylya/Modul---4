a72c852ce0bcce6b0c530e376194531705eb0827
Лена <trylya@mail.ru>
2024-10-05 22:30:08 +0300

    деление

--- module_4_1.py
+++ module_4_1.py
@@ -1,6 +1,5 @@
-from fake_math import divide as d1
-from true_math import divide as d2
-
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
 
 
 result1 = fake_divide(69, 3)
